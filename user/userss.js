8417151d0a1bb3d3b13392af7d9846239a465d4e
Abir-Ayed <abirayed277@gmail.com>
2021-12-20 21:38:20 +0100

    added my project

---
+++ user/userss.js
@@ -0,0 +1,19 @@
+const userss = [
+    {
+		id: 1,
+		name: "abir",
+        age:24
+	},
+    {
+			id: 2,
+			name: "ayed",
+            age:24
+	},
+    {
+        id: 3,
+        name: "aayed",
+        age:24
+}
+];
+
+exports.userss = userss;
